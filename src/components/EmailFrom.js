0dd3aa804d7445f6269df6525224405b09136377
sovik89 <soviwizz@gmail.com>
2023-10-25 22:58:11 +0530

    Mail Sent Id

---
+++ src/components/EmailFrom.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import emailjs from '@emailjs/browser';
+
+const EmailForm = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Your EmailJS service ID, template ID, and Public Key
+    const serviceId = 'service_egosxci';
+    const templateId = 'template_zb0hjio';
+    const publicKey = 'Eme45JLZHbmkh4wap';
+
+    // Create a new object that contains dynamic template params
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: 'Ante',
+      message: message,
+    };
+
+    // Send the email using EmailJS
+    emailjs.send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log('Email sent successfully!', response);
+        setName('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+      });
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className='emailForm'>
+      <input
+        type="text"
+        placeholder="Your Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      
+      <input
+        type="email"
+        placeholder="Your Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <textarea
+        cols="30"
+        rows="10"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      >
+      </textarea>
+      <button type="submit">Send Email</button>
+    </form>
+  )
+}
+
+export default EmailForm
